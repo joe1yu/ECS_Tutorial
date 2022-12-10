8ab10c86072d023ba81567825a10f6d63d9e55de
joelyu <joe1yu@163.com>
2022-12-11 03:12:52 +0800

    Init commit

---
+++ Assets/Scripts/Authoring/TankAuthoring.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+
+class TankAuthoring : UnityEngine.MonoBehaviour
+{
+    
+}
+
+class TankBaker : Baker<TankAuthoring>
+{
+    public override void Bake(TankAuthoring authoring)
+    {
+        AddComponent<Tank>();
+    }
+}
---
+++ Assets/Scripts/Components/Config.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+struct Config : IComponentData
+{
+    public Entity TankPrefab;
+    public int TankCount;
+    public float SafeZoneRadius;
+}
